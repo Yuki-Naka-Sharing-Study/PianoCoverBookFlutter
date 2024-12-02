c1b00cb864384a0f7a812e4d78cd4b49da049ba3
仲優樹 <yu-ki216m@outlook.jp>
2024-12-02 21:41:40 +0900

    Created TextField for song title, composer name, and notes. Also created texts for right hand proficiency and left hand proficiency.

--- lib/src/screens/record.dart
+++ lib/src/screens/record.dart
@@ -2,15 +2,116 @@ import 'package:flutter/material.dart';
 
 class RecordScreen extends StatelessWidget {
   const RecordScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('記録する'),
       ),
-      body: const Center(
-          child: Text('', style: TextStyle(fontSize: 32.0))),
+      body: const Column(
+        children: [
+          Row(
+            children: [
+              SizedBox(width: 16),
+              Text("曲名"),
+              SizedBox(width: 16),
+              Expanded(
+                child: TextField(
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.normal,
+                  ),
+                  decoration: InputDecoration(
+                    hintText: 'ラ・カンパネラ',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              SizedBox(width: 16),
+            ],
+          ),
+
+          SizedBox(height: 8),
+
+          Row(
+            children: [
+              SizedBox(width: 16),
+              Text("作曲者名"),
+              SizedBox(width: 16),
+              Expanded(
+                child: TextField(
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.normal,
+                  ),
+                  decoration: InputDecoration(
+                    hintText: 'リスト',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              SizedBox(width: 16),
+            ],
+          ),
+
+          SizedBox(height: 8),
+
+          Row(
+            children: [
+              SizedBox(width: 16),
+              Text("メモ"),
+              SizedBox(width: 16),
+              Expanded(
+                child: TextField(
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.normal,
+                  ),
+                  decoration: InputDecoration(
+                    hintText: 'アルペジオが難しい。',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              SizedBox(width: 16),
+            ],
+          ),
+
+          Text(
+            '右手の習熟度',
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+
+          SizedBox(height: 4),
+
+          // ○○％のCircular Progress Bar
+
+
+          // ○○％のSeekBar
+
+          Text(
+            '左手の習熟度',
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+
+          SizedBox(height: 4),
+
+          // ○○％のCircular Progress Bar
+
+
+          // ○○％のSeekBar
+
+
+        ],
+
+      ),
     );
   }
 }
